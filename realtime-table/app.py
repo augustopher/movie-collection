802123d5c2c59d149104665ad15ea1fd24e19f0f
Gus Powers <guspowers@me.com>
2019-07-16 04:00:44 -0400

    adding database table & schema

--- realtime-table/app.py
+++ realtime-table/app.py
@@ -3,6 +3,8 @@ import os
 import json
 import pusher
 from datetime import datetime
+from database import db_session
+from models import Flight
 
 app = Flask(__name__)
 
@@ -10,6 +12,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
 
 
 if __name__ == '__main__':
