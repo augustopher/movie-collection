82f2be2c0cb39b32849cf869b918968e880a7ab3
Gus Powers <guspowers@me.com>
2019-07-16 03:53:50 -0400

    setting up database

--- realtime-table/app.py
+++ realtime-table/app.py
@@ -3,3 +3,14 @@ import os
 import json
 import pusher
 from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
