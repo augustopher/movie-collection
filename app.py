861a8cefc7b67589bb361e2f11825da1a85fcc54
Gus Powers <guspowers@me.com>
2020-03-24 22:21:38 -0400

    add basic table definiton

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
-database_file = f"sqlite:///{os.path.join(project_dir, movies.db)}"
+database_file = "sqlite:///{}".format(os.path.join(project_dir, movies.db))
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = database_file
@@ -18,3 +18,10 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+class Movie(db.Model):
+    self.movie_id = db.Column(db.Integer, primary_key=True)
+    self.movie_title = db.Column(db.String(200))
+    
+    def __repr__(self):
+        return "<Title: {}>".format(self.movie_title)
