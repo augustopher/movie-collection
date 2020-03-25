fe0c187f110c484e2623f0d2f75bd99d556146ad
Gus Powers <guspowers@me.com>
2020-03-24 22:27:24 -0400

    fix path join

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
-database_file = "sqlite:///{}".format(os.path.join(project_dir, movies.db))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, 'movies.db'))
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = database_file
@@ -13,15 +13,17 @@ db = SQLAlchemy(app)
 @app.route('/', methods=['GET','POST'])
 def home():
     if request.form:
-        print(request.form)
+        movie = Movie(movie_title=request.form.get('title'))
+        db.session.add(movie)
+        db.session.commit()
     return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
 
 class Movie(db.Model):
-    self.movie_id = db.Column(db.Integer, primary_key=True)
-    self.movie_title = db.Column(db.String(200))
+    movie_id = db.Column(db.Integer, primary_key=True)
+    movie_title = db.Column(db.String(200))
     
     def __repr__(self):
         return "<Title: {}>".format(self.movie_title)
