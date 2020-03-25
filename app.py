47b5eff5131311715132babae7c1eec40c354892
Gus Powers <guspowers@me.com>
2020-03-24 22:08:10 -0400

    add backbone for database

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import os
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = f"sqlite:///{os.path.join(project_dir, movies.db)}"
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = database_file
+
+db = SQLAlchemy(app)
 
 @app.route('/', methods=['GET','POST'])
 def home():
