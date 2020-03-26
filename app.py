3321ed680db6e684361498282be0001383deac5e
Gus Powers <guspowers@me.com>
2020-03-25 20:28:14 -0400

    add error handling

--- app.py
+++ app.py
@@ -20,21 +20,29 @@ class Movie(db.Model):
 @app.route('/', methods=['GET','POST'])
 def home():
     if request.form:
-        movie = Movie(movie_title=request.form.get('title'))
-        db.session.add(movie)
-        db.session.commit()
+        try:
+            movie = Movie(movie_title=request.form.get('title'))
+            db.session.add(movie)
+            db.session.commit()
+        except Exception as e:
+            print("Failed to add movie")
+            print(e)
     
     movie_list = Movie.query.all()
     return render_template('home.html', movie_list=movie_list)
 
 @app.route('/update', methods=['POST'])
 def update():
-    old_title = request.form.get('old_title')
-    new_title = request.form.get('new_title')
-    
-    movie = Movie.query.filter_by(movie_title=old_title).first()
-    movie.movie_title = new_title
-    db.session.commit()
+    try:
+        old_title = request.form.get('old_title')
+        new_title = request.form.get('new_title')
+        
+        movie = Movie.query.filter_by(movie_title=old_title).first()
+        movie.movie_title = new_title
+        db.session.commit()
+    except Exception as e:
+        print("Couldn't update movie title")
+        print(e)
     
     return redirect('/')
 
